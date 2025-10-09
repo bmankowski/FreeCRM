e1f9ca0ae804a60e8d1baed835c7b3376e1dff07
bmankowski <bmankowski@gmail.com>
2025-10-09 15:32:01 +0200

    Migrate API to PSR-4 (with 4 minor issues)

---
+++ src/Modules/API/Handlers/CalDAV.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FreeCRM\Modules\API\Handlers;
+
+/**
+ * Api CalDAV Handler Class
+ * @package YetiForce.Handler
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class Handler {
+
+	/**
+	 * EntityAfterSave handler function
+	 * @param App\EventHandler $eventHandler
+	 */
+	public function entityAfterSave(App\EventHandler $eventHandler)
+	{
+		$recordModel = $eventHandler->getRecordModel();
+		if (!$recordModel->isNew()) {
+			\App\Db::getInstance()->createCommand()->update('vtiger_activity', ['dav_status' => 1], ['activityid' => $recordModel->getId()])->execute();
+		}
+	}
+}
