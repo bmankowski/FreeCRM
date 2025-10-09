fdb08a52f62d2c941e76bab1543ca3705eb9b0a4
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:40 +0200

    Migrate SCalculations to PSR-4

---
+++ src/Modules/SCalculations/Models/Record.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FreeCRM\Modules\SCalculations\Models;
+
+/**
+ * Record Class for SCalculations
+ * @package YetiForce.Model
+ * @license licenses/License.html
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+class Record extends Model
+{
+
+	protected $privileges = ['editFieldByModal' => true];
+
+	public function getFieldToEditByModal()
+	{
+		return [
+			'addClass' => 'btn-default',
+			'iconClass' => 'glyphicon-modal-window',
+			'listViewClass' => '',
+			'titleTag' => 'LBL_SET_RECORD_STATUS',
+			'name' => 'scalculations_status',
+		];
+	}
+}
