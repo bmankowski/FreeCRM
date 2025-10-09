90b8173df1d87e8d71c6aae7db170f481c81cd26
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:39 +0200

    Migrate SRequirementsCards to PSR-4

---
+++ src/Modules/SRequirementsCards/Models/Record.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FreeCRM\Modules\SRequirementsCards\Models;
+
+/**
+ * Record Class for SRequirementsCards
+ * @package YetiForce.Model
+ * @license licenses/License.html
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+class Record extends Model
+{
+
+	protected $privileges = ['editFieldByModal' => true];
+
+	public function getFieldToEditByModal()
+	{
+		return [
+			'addClass' => 'btn-default',
+			'iconClass' => 'glyphicon-modal-window',
+			'listViewClass' => '',
+			'titleTag' => 'LBL_SET_RECORD_STATUS',
+			'name' => 'srequirementscards_status',
+		];
+	}
+}
