e83ff2b4c9ecee223ac774d29f7f0610ab4f3981
bmankowski <bmankowski@gmail.com>
2025-10-09 15:30:46 +0200

    Migrate Faq to PSR-4

---
+++ src/Modules/Faq/Models/ListView.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FreeCRM\Modules\Faq\Models;
+
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
+
+/**
+ * Vtiger ListView Model Class
+ */
+class ListView extends Model
+{
+	/*
+	 * Function to give advance links of a module
+	 * 	@RETURN array of advanced links
+	 */
+
+	public function getAdvancedLinks()
+	{
+		return array();
+	}
+}
+
+?>
