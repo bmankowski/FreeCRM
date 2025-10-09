4407eb86edb78b2c318b793da50abe9723dbf8d3
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:08 +0200

    Migrate OSSEmployees to PSR-4

---
+++ src/Modules/OSSEmployees/Models/Relation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace FreeCRM\Modules\OSSEmployees\Models;
+
+/**
+ * Relation Model
+ * @package YetiForce.Model
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class Relation extends Model
+{
+
+	/**
+	 * Get time control
+	 */
+	public function getOsstimecontrol()
+	{
+		$this->getQueryGenerator()->addNativeCondition(['vtiger_crmentity.smownerid' => $this->get('parentRecord')->get('assigned_user_id')]);
+	}
+}
