f413ec162d80f8d7bacda92edcb0852d1ac6c00f
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:38 +0200

    Migrate SSingleOrders to PSR-4

---
+++ src/Modules/SSingleOrders/Views/EditFieldByModal.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FreeCRM\Modules\SSingleOrders\Views;
+
+/**
+ * EditFieldByModal View Class
+ * @package YetiForce.View
+ * @license licenses/License.html
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+
+use FreeCRM\Http\Vtiger_Request;
+class EditFieldByModal extends View
+{
+
+	protected $restrictItems = ['PLL_CANCELLED' => 'btn-danger', 'PLL_ACCEPTED' => 'btn-success'];
+
+	public function getConditionToRestricts($moduleName, $ID)
+	{
+		return Users_Privileges_Model::isPermitted($moduleName, 'CloseRecord', $ID);
+	}
+}
