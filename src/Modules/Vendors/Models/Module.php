29b8143ebd7f906340bc9d2d89d7373caa537cb2
bmankowski <bmankowski@gmail.com>
2025-10-09 15:30:38 +0200

    Migrate Vendors to PSR-4

---
+++ src/Modules/Vendors/Models/Module.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FreeCRM\Modules\Vendors\Models;
+
+/**
+ * Vendors module model Class
+ * @package YetiForce.Model
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class Module extends Model
+{
+
+	/**
+	 * Function to get list view query for popup window
+	 * @param string $sourceModule Parent module
+	 * @param string $field parent fieldname
+	 * @param string $record parent id
+	 * @param \App\QueryGenerator $queryGenerator
+	 */
+	public function getQueryByModuleField($sourceModule, $field, $record, \App\QueryGenerator $queryGenerator)
+	{
+		if ($sourceModule == 'Campaigns') {
+			$subQuery = (new App\Db\Query())
+				->select(['crmid'])
+				->from('vtiger_campaign_records')
+				->where(['campaignid' => $record]);
+			$queryGenerator->addNativeCondition(['not in', 'vtiger_vendor.vendorid', $subQuery]);
+		}
+	}
+}
---
+++ src/Modules/Vendors/Models/Relation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FreeCRM\Modules\Vendors\Models;
+
+/**
+ * Relation Model
+ * @package YetiForce.Model
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class Relation extends Model
+{
+
+	/**
+	 * Get products
+	 */
+	public function getProducts()
+	{
+		$queryGenerator = $this->getQueryGenerator();
+		$queryGenerator->addJoin(['INNER JOIN', 'vtiger_vendor', 'vtiger_vendor.vendorid = vtiger_products.vendor_id']);
+		$queryGenerator->addNativeCondition(['vtiger_vendor.vendorid' => $this->get('parentRecord')->getId()]);
+	}
+}
