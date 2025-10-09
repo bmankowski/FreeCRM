9b7122a1af81cc41372813b082f5151b1bf762dc
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:10 +0200

    Migrate OSSOutsourcedServices to PSR-4

---
+++ src/Modules/OSSOutsourcedServices/Models/TreeView.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace FreeCRM\Modules\OSSOutsourcedServices\Models;
+
+/**
+ * OSSOutsourcedServices TreeView Model Class
+ * @package YetiForce.TreeView
+ * @license licenses/License.html
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+class TreeView extends Model
+{
+
+	public function isActive()
+	{
+		return true;
+	}
+}
