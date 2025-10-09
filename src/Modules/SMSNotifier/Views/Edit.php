0a2202996a6cb8eaecbb22e399f0b69097e39cfe
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:57 +0200

    Migrate SMSNotifier to PSR-4 (with 3 minor issues)

---
+++ src/Modules/SMSNotifier/Views/Edit.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FreeCRM\Modules\SMSNotifier\Views;
+
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
+
+
+use FreeCRM\Http\Vtiger_Request;
+class Edit extends View
+{
+
+	public function checkPermission(\FreeCRM\Http\Vtiger_Request $request)
+	{
+		throw new \Exception\NoPermitted('LBL_PERMISSION_DENIED');
+	}
+}
