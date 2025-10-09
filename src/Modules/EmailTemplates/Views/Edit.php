f1e008bed263cf3cfe634e6ca3a9689e5d3bddf0
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:57 +0200

    Migrate EmailTemplates to PSR-4

---
+++ src/Modules/EmailTemplates/Views/Edit.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FreeCRM\Modules\EmailTemplates\Views;
+
+class Edit extends View
+{
+
+	/**
+	 * Function to get the list of Script models to be included
+	 * @param Vtiger_Request $request
+	 * @return array - List of Vtiger_JsScript_Model instances
+	 */
+	public function getFooterScripts(\FreeCRM\Http\Vtiger_Request $request)
+	{
+		$parentScript = parent::getFooterScripts($request);
+		$fileNames = [
+			'libraries.jquery.clipboardjs.clipboard',
+		];
+		$scriptInstances = $this->checkAndConvertJsScripts($fileNames);
+		return array_merge($parentScript, $scriptInstances);
+	}
+}
