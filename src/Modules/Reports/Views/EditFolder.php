37e5385e37f549c5a0adae66ebd71ae77a4fe8ad
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:48 +0200

    Migrate Reports to PSR-4 (with 6 minor issues)

---
+++ src/Modules/Reports/Views/EditFolder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace FreeCRM\Modules\Reports\Views;
+
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): YetiForce.com
+ * ********************************************************************************** */
+
+
+use FreeCRM\Http\Vtiger_Request;
+class EditFolder extends View
+{
+
+	public function checkPermission(\FreeCRM\Http\Vtiger_Request $request)
+	{
+		$currentUserPriviligesModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
+		if (!$currentUserPriviligesModel->hasModulePermission($request->getModule())) {
+			throw new \Exception\NoPermitted('LBL_PERMISSION_DENIED');
+		}
+	}
+
+	public function process(\FreeCRM\Http\Vtiger_Request $request)
+	{
+
+		$viewer = $this->getViewer($request);
+		$moduleName = $request->getModule();
+		$folderId = $request->get('folderid');
+
+		if ($folderId) {
+			$folderModel = Reports_Folder_Model::getInstanceById($folderId);
+		} else {
+			$folderModel = Reports_Folder_Model::getInstance();
+		}
+
+		$viewer->assign('FOLDER_MODEL', $folderModel);
+		$viewer->assign('MODULE', $moduleName);
+		$viewer->view('EditFolder.tpl', $moduleName);
+	}
+}
---
+++ src/Modules/Reports/Views/ListAjax.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace FreeCRM\Modules\Reports\Views;
+
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is: vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * ********************************************************************************** */
+/*
+ * Class for reloading the Reports headers
+ */
+
+
+use FreeCRM\Http\Vtiger_Request;
+class ListAjax extends View
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->exposeMethod('getListViewCount');
+		$this->exposeMethod('getRecordsCount');
+		$this->exposeMethod('getPageCount');
+	}
+
+	public function preProcess(\FreeCRM\Http\Vtiger_Request $request, $display = true)
+	{
+		
+	}
+
+	public function process(\FreeCRM\Http\Vtiger_Request $request)
+	{
+		$mode = $request->get('mode');
+		if (!empty($mode)) {
+			$this->invokeExposedMethod($mode, $request);
+			return;
+		}
+		$viewer = $this->getViewer($request);
+		$moduleName = $request->getModule();
+		$moduleModel = Vtiger_Module_Model::getInstance($moduleName);
+
+		$folders = $moduleModel->getFolders();
+		$listViewModel = new Reports_ListView_Model();
+		$listViewModel->set('module', $moduleModel);
+
+		$linkModels = $listViewModel->getListViewLinks(false);
+
+		$viewer->assign('LISTVIEW_LINKS', $linkModels);
+		$viewer->assign('FOLDERS', $folders);
+
+		$viewer->view('ListViewFolders.tpl', $moduleName);
+	}
+}
