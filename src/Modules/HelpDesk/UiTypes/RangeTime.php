e779ae341a4d1658ca534af76ea14004c301bec9
bmankowski <bmankowski@gmail.com>
2025-10-09 15:30:41 +0200

    Migrate HelpDesk to PSR-4 (with 2 minor issues)

---
+++ src/Modules/HelpDesk/UiTypes/RangeTime.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace FreeCRM\Modules\HelpDesk\UiTypes;
+
+/**
+ * UIType RangeTime Field Class
+ * @package YetiForce.Fields
+ * @license licenses/License.html
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+class RangeTime extends UIType
+{
+
+	public function getDisplayValue($value, $record = false, $recordInstance = false, $rawText = false)
+	{
+		$isNull = is_null($value);
+		if ($this->get('field')->getName() == 'response_time') {
+			$value = round(vtlib\Functions::getDateTimeMinutesDiff($value, date('Y-m-d H:i:s')));
+		}
+		$result = vtlib\Functions::getRangeTime($value, !$isNull);
+		$mode = $this->get('field')->getFieldParams();
+		if (empty($mode)) {
+			$mode = 'short';
+		}
+		return $result[$mode];
+	}
+}
