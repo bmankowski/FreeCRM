0fea9bc643cd3f264aeef92e4ed87175d207b4ee
bmankowski <bmankowski@gmail.com>
2025-10-09 15:31:38 +0200

    Migrate SRecurringOrders to PSR-4

---
+++ src/Modules/SRecurringOrders/Models/Record.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FreeCRM\Modules\SRecurringOrders\Models;
+
+/**
+ * Record Class for SRecurringOrders
+ * @package YetiForce.Model
+ * @license licenses/License.html
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+class Record extends Model
+{
+
+	protected $privileges = ['editFieldByModal' => true];
+
+	public function getFieldToEditByModal()
+	{
+		return [
+			'addClass' => 'btn-default',
+			'iconClass' => 'glyphicon-modal-window',
+			'listViewClass' => '',
+			'titleTag' => 'LBL_SET_RECORD_STATUS',
+			'name' => 'srecurringorders_status',
+		];
+	}
+}
