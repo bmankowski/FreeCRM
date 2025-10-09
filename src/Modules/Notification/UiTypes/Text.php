9318a6b5d664e930972751927cd10987769b65a4
bmankowski <bmankowski@gmail.com>
2025-10-09 15:30:11 +0200

    Migrate Notification to PSR-4 (with 3 minor issues)

---
+++ src/Modules/Notification/UiTypes/Text.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FreeCRM\Modules\Notification\UiTypes;
+
+/**
+ * Uitype Model
+ * @package YetiForce.Github
+ * @license licenses/License.html
+ * @author Tomasz Kur <t.kur@yetiforce.com>
+ */
+class Text extends UIType
+{
+
+	/**
+	 * Function to get the Display Value, for the current field type with given DB Insert Value
+	 * @param <Object> $value
+	 * @return <Object>
+	 */
+	public function getDisplayValue($value, $record = false, $recordInstance = false, $rawText = false)
+	{
+		return nl2br($recordInstance->getParseField($this->get('field')->getName()));
+	}
+}
---
+++ src/Modules/Notification/Views/Reminders.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FreeCRM\Modules\Notification\Views;
+
+/**
+ * Notifications reminders
+ * @package YetiForce.View
+ * @license licenses/License.html
+ * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+
+/**
+ * Class for Notifications reminders
+ */
+
+use FreeCRM\Http\Vtiger_Request;
+class Reminders extends View
+{
+
+	/**
+	 * Process
+	 * @param Vtiger_Request $request
+	 */
+	public function process(\FreeCRM\Http\Vtiger_Request $request)
+	{
+		$viewer = $this->getViewer($request);
+		$moduleName = $request->getModule();
+		$moduleModel = Vtiger_Module_Model::getInstance($moduleName);
+		$entries = $moduleModel->getEntries(\FreeCRM\AppConfig::module($moduleName, 'MAX_NUMBER_NOTIFICATIONS'));
+		$colors = ['PLL_SYSTEM' => '#FF9800', 'PLL_USERS' => '#1baee2'];
+		$viewer->assign('RECORDS', $entries);
+		$viewer->assign('COLORS', $colors);
+		$viewer->view('Reminders.tpl', $moduleName);
+	}
+}
