c581f211e2305d885ff33570f3793a77f8816244
bmankowski <bmankowski@gmail.com>
2025-11-13 11:37:21 +0100

    refactor: Enhance ListView class by restructuring preProcess method and adding new data assignments
    - Updated the preProcess method to call the parent method and streamline the initialization process.
    - Introduced new viewer assignments for ALL_LIBRARIES and SYSTEM_MODE to improve data handling.
    - Added a separate process method to handle AJAX requests more effectively, enhancing overall functionality and maintainability of the ListView implementation.

--- src/Modules/Settings/ModuleManager/Views/ListView.php
+++ src/Modules/Settings/ModuleManager/Views/ListView.php
@@ -15,19 +15,26 @@ namespace App\Modules\Settings\ModuleManager\Views;
 class ListView extends \App\Modules\Settings\Base\Views\Index
 {
 
-	public function process(\App\Http\Vtiger_Request $request)
+	public function preProcess(\App\Http\Vtiger_Request $request, $display = true)
 	{
+		parent::preProcess($request, false);
 		$viewer = $this->getViewer($request);
 		$moduleName = $request->getModule();
 		$qualifiedModuleName = $request->getModule(false);
-
+	
 		$viewer->assign('ALL_MODULES', \App\Modules\Settings\ModuleManager\Models\Module::getAll());
 		$viewer->assign('RESTRICTED_MODULES_LIST', \App\Modules\Settings\ModuleManager\Models\Module::getActionsRestrictedModulesList());
 		$viewer->assign('IMPORT_MODULE_URL', \App\Modules\Settings\ModuleManager\Models\Module::getNewModuleImportUrl());
 		$viewer->assign('IMPORT_USER_MODULE_URL', \App\Modules\Settings\ModuleManager\Models\Module::getUserModuleImportUrl());
-		$viewer->assign('MODULE', $moduleName);
-		$viewer->assign('USER_MODEL', $request->getUser());
-
+		$viewer->assign('ALL_LIBRARIES', \App\Modules\Settings\ModuleManager\Models\Library::getAll());
+		$viewer->assign('SYSTEM_MODE', \App\AppConfig::main('systemMode'));
+	}
+	
+	public function process(\App\Http\Vtiger_Request $request)
+	{
+		$viewer = $this->getViewer($request);
+		$qualifiedModuleName = $request->getModule(false);
+	
 		if ($request->isAjax()) {
 			// AJAX request - return content only
 			$viewer->view('ListContent.tpl', $qualifiedModuleName);
