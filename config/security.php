f0cb0a91f5bfc835a9e1a27861691de64aec2358
bmankowski <bmankowski@gmail.com>
2025-11-18 07:52:17 +0100

    refactor: Update security configuration to enable password reset and user preferences
    This commit modifies the security configuration to allow password resets during login and to show user preferences. These changes enhance user experience by providing more flexibility in account management settings.

--- config/security.php
+++ config/security.php
@@ -2,9 +2,9 @@
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $SECURITY_CONFIG = [
 	// Possible to reset the password while logging in (true/false)
-	'RESET_LOGIN_PASSWORD' => false,
+	'RESET_LOGIN_PASSWORD' => true,
 	// Show my preferences
-	'SHOW_MY_PREFERENCES' => false,
+	'SHOW_MY_PREFERENCES' => true,
 	// Changing the settings by the user is possible true/false
 	'CHANGE_LOGIN_PASSWORD' => true,
 	/*
